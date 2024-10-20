e069a42f45bc620e9ac61153dcd1a6c13a2ca988
RazaqFarhanR <aljawadfarhan@gmail.com>
2024-10-20 17:18:01 +0700

    feat: add colors app

---
+++ lib/constanst/app_colors.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color DarkBlue = Color.fromRGBO(7, 25, 82, 1);
+  static const Color MediumBlue = Color.fromRGBO(8, 131, 149, 1);
+  static const Color LightBlue = Color.fromRGBO(55, 183, 195, 1);
+  static const Color LigthGrey = Color.fromRGBO(235, 244, 246, 1);
+}
